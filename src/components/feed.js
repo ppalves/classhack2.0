a0ff1edb895c298f7c6660e805d89904f61c59c4
Pedro Pupo <pedro.pupo@conpec.com.br>
2019-08-24 13:57:48 -0300

    config pages

---
+++ src/components/feed.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Feed extends React.Component {
+  render () {
+    return(
+      <h1>baba</h1>
+    )
+
+  }
+}
+
+export default Feed;
